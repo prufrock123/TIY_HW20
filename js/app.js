267aa806745e25100df6b27bc639f75ef733f941
prufrock123 <shawnmllr86@gmail.com>
2014-10-19 20:37:05 -0500

    trying to get the images interpolated in the templates.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
+// The code below correctly gets all the active listings from Etsy.
+	// var etsy_url = "https://openapi.etsy.com/";
+	// var version = "v2/";
+	// var model = "listings/";
+	// var filter = "active";
+	// var js = ".js";
+	// var api_key = "aavnvygu0h5r52qes74x9zvo";
+	// var complete_api_url = etsy_url + version;
 
-var etsy_url = "https://openapi.etsy.com/";
-var version = "v2/";
-var model = "listings/";
-var filter = "active";
-var js = ".js";
-var api_key = "aavnvygu0h5r52qes74x9zvo";
-var complete_api_url = etsy_url + version;
-
-$.getJSON(complete_api_url + model + filter + js + "?" + "api_key=" + api_key + "&callback=?").then(function(data){
-    console.log(data);
-});
+	// $.getJSON(complete_api_url + model + filter + js + "?" + "api_key=" + api_key + "&callback=?").then(function(data){
+	//     console.log(data);
+	// });
 
 
 // so when we create a new EtsyClient instance, we will be passing an options object
@@ -25,19 +25,75 @@ function EtsyClient(options) {
     this.version = options.api_version || "v2/";
     this.api_key = options.api_key;
     this.complete_api_url = this.etsy_url + this.version;
+
+    // this.getAllActiveListings();
+    this.init();
 }
 
-EtsyClient.prototype.pullAllActiveListings = function() {
-    var model = 'listings';
-    var filter = 'active';
-    return $.getJSON(this.complete_api_url + model + filter + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
-        console.log(data);
-    });
+// Get data functions
+	// Get all active listings.
+		EtsyClient.prototype.getAllActiveListings = function() {
+		    var model = 'listings/'; // the forward slash was missing from the notes code.
+		    var filter = 'active';
+		    return $.getJSON(this.complete_api_url + model + filter + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
+		        // console.log(data);
+		        return data;
+		    });
+		};
+
+
+	// Get individual listing
+		// EtsyClient.prototype.getListingInfo = function(id) {
+		//     var model = 'listings';
+		//     return $.getJSON(this.complete_api_url + model + '/' + id + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
+		//         console.log(data);
+		//     });
+		// };
+
+// Get HTML templates function
+	EtsyClient.prototype.loadTemplateFile = function(templateName) {
+		return $.get('templates/' + templateName + '.html')
+		.then(function(htmlString){
+			return htmlString;
+		});
+	};
+
+
+// Add data to HTML functions
+	// Add all active listings on page
+		EtsyClient.prototype.addAllActiveListingsToPage = function(html, data){
+			// console.log(data);
+			// console.log(html);
+			document.querySelector('#all_listings').innerHTML = 
+			data.results.map(function(element){
+				return _.template(html, element);
+			}).join("");
+		};
+	
+	
+
+EtsyClient.prototype.init = function() {
+	var self = this;
+	$.when(
+		this.getAllActiveListings(),
+		this.loadTemplateFile('allListings')
+	).then(function(allActiveListings, allListingsHtml){
+		self.addAllActiveListingsToPage(allListingsHtml, allActiveListings)
+	});
+};
+
+window.onload = app;
+
+function app() {
+	var etsy = new EtsyClient({ api_key: "ab2ph0vvjrfql5ppli3pucmw" })
 }
 
-EtsyClient.prototype.getListingInfo = function(id) {
-    var model = 'listings';
-    return $.getJSON(this.complete_api_url + model + '/' + id + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
-        console.log(data);
-    });
-}
+
+
+
+
+
+
+
+
+
